b0d397e3727173eaf83d8d2df82cc044ba261a03
Elis Vathi <elisvathi@outlook.com>
2023-02-04 23:44:19 +0100

    fix camera

--- src/startup/camera.rs
+++ src/startup/camera.rs
@@ -112,7 +112,7 @@ fn get_primary_window_size(windows: &Res<Windows>) -> Vec2 {
 
 /// Spawn a camera like this
 fn spawn_camera(mut commands: Commands) {
-    let translation = Vec3::new(-2.0, 2.5, 5.0);
+    let translation = Vec3::new(-2.0, 10.5, 5.0);
     let radius = translation.length();
 
     commands.spawn((
